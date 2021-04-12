b1b25258a89817f34a52005f9d08bddcfdde4635
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-04-12 11:55:50 -0300

    Sequelize - CRUD

--- instagram.js
+++ instagram.js
@@ -2,6 +2,36 @@ const { Usuario, Post, sequelize, Comentario } = require('./models');
 const { Op } = require('sequelize');
 
 //Usuario
+
+// Usuario.create({
+//   nome: 'Rogerio Parízio',
+//   email: 'rogerio@email.com',
+//   senha: 'rogerio123',
+// }).then((resultado) => {
+//   console.table(resultado.toJSON());
+// });
+
+// Usuario.update(
+//   {
+//     email: 'sergio@digitalhouse.com',
+//   },
+//   {
+//     where: {
+//       id: 2,
+//     },
+//   }
+// ).then((resultado) => {
+//   console.log(resultado);
+// });
+
+// Usuario.destroy({
+//   where: {
+//     id: 3,
+//   },
+// }).then((resultado) => {
+//   console.log(resultado);
+// });
+
 // Usuario.findAll({
 //   where: {
 //     nome: { [Op.like]: '%a%' },
@@ -18,6 +48,10 @@ const { Op } = require('sequelize');
 //   console.table(resultado.map((user) => user.toJSON()));
 // });
 
+// Usuario.findAll().then((resultado) => {
+//   console.table(resultado.map((user) => user.toJSON()));
+// });
+
 // Usuario.findOne({
 //   where: {
 //     nome: 'Gabriel Brunetti',
@@ -31,6 +65,14 @@ const { Op } = require('sequelize');
 // });
 
 //POST;
+
+// Post.create({
+//   texto: 'NodeJS é top de bola!',
+//   usuarios_id: 7,
+//   n_likes: 1,
+// }).then((resultado) => {
+//   console.log(resultado.toJSON());
+// });
 
 // Post.findAll({
 //   order: [['texto', 'ASC']],
@@ -52,16 +94,20 @@ const { Op } = require('sequelize');
 //   console.log(resultado.toJSON());
 // });
 
+// Post.findAll({}).then((resultado) => {
+//   console.log(resultado.map((post) => post.toJSON()));
+// });
+
 // Comentario
-for (let i = 0; i < 6; i += 2) {
-  Comentario.findAll({
-    order: [['id', 'DESC']],
-    offset: i,
-    limit: 2,
-  }).then((resultado) => {
-    console.table(resultado.map((comment) => comment.toJSON()));
-  });
-}
+// for (let i = 0; i < 6; i += 2) {
+//   Comentario.findAll({
+//     order: [['id', 'DESC']],
+//     offset: i,
+//     limit: 2,
+//   }).then((resultado) => {
+//     console.table(resultado.map((comment) => comment.toJSON()));
+//   });
+// }
 
 // Comentario.findOne({
 //   where: {
